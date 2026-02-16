bf6cbcd02acb7ec6196b98155e336a48f4ef70c1
Shumaila550 <codecanvas78@gmail.com>
2026-02-16 11:29:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function scrollToSection(id){
-  document.getElementById(id).scrollIntoView({
-    behavior:'smooth'
+document.querySelectorAll("nav a").forEach(link=>{
+  link.addEventListener("click",e=>{
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({behavior:"smooth"});
   });
-}
+});
